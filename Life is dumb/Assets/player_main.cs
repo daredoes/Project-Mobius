66ec9b69e0401235487a636334cedc44e265f1d5
Daniel Evans <me@danielarevans.com>
2015-09-22 21:23:37 -0700

    Major Refresh
    I replaced the “Project Mobius” Folder with my already working “Life is
    dumb” folder. Everything works now.

---
+++ Life is dumb/Assets/player_main.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_main : MonoBehaviour {
+	private GameObject[] beatBars;
+	private Vector2 middleBeatPosition = new Vector2(0, -1);
+	private float howBarApart = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void generateBeatBars(int count)
+	{
+		beatBars = new GameObject[count];
+		for (int i = 0; i < count; i++) {
+
+		}
+	}
+	void Awake(){
+
+		///barCount = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
