f3216e849d30fb3e89b2775225e28dbd6c0d66b3
Daniel Evans <me@danielarevans.com>
2015-09-23 17:43:40 -0700

    Solving Problems

--- Life is dumb/Assets/Scripts/button.cs
+++ Life is dumb/Assets/Scripts/button.cs
@@ -1,73 +1,17 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class button : MonoBehaviour {
-
-    public bool p1;
-    public bool barInstantiated;
-
-    static float barDist = 0.5f;
-
-    //BeatBar Prefab
+	public bool p1;
 	public GameObject beatBar;
 
-    //Refrence GameObject (this beatButtons BeatBar)
-    public GameObject myBar;
-
-    //Refrence to Beat Bouncer Script
-    beatBouncer _bb;
-
-    void Awake()
-    {
-        
-    }
-
-    void Start()
-    {
-        if(gameObject.activeInHierarchy)
-        {
-            if(!barInstantiated)
-            {
-                InstantiateBar();
-            }
-        }
-    }
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
-            {
-                if (myBar != null)
-                {
-                    myBar.SendMessage("hit");
-                }                
-            }
-
-        }
-
-    }
-
-    void InstantiateBar()
-    {
-        Vector3 Pos = transform.position;
-        if (p1)
-        {
-            myBar = (GameObject)Instantiate(beatBar);
-            myBar.GetComponent<beatBouncer>().p1 = p1;
-            myBar.transform.SetParent(this.transform);
-            myBar.transform.position = new Vector3(Pos.x, Pos.y + (barDist * 1), Pos.z);
-            barInstantiated = true;
-            
-        }
-        else if (!p1)
-        {
-            myBar = (GameObject)Instantiate(beatBar);
-            myBar.GetComponent<beatBouncer>().p1 = p1;
-            myBar.transform.SetParent(this.transform);
-            myBar.transform.position = new Vector3(Pos.x, Pos.y + (barDist * -1), Pos.z);
-            barInstantiated = true;
-        }
-    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
