5088a529e89acb22b7218febf06951695ee6828a
Cristian Ponce <christian.rponce@gmail.com>
2015-09-23 18:18:42 -0700

    deleted life is dumb......

---
+++ Project Mobius/Assets/Scripts/character_movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character_movement : MonoBehaviour {
+	private Rigidbody2D body;
+	Transform trans;
+	float moveStep = .2f;
+	int push = 25;
+	// Use this for initialization
+	void Start () {
+		trans = gameObject.transform;
+		body = gameObject.GetComponent<Rigidbody2D> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)) {
+			body.AddForce(new Vector2(0, push));
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			body.AddForce(new Vector2(push*-1, 0));
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			body.AddForce(new Vector2(0, push*-1));
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			body.AddForce(new Vector2(push, 0));
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			body.simulated = !body.simulated;
+		}
+	
+	}
+}
