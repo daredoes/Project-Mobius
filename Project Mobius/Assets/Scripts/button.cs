a096375d2b0f1174da4347ecc1ab5fd1a41d515e
Daniel Evans <me@danielarevans.com>
2015-10-05 15:45:54 -0700

    Touch Drag

--- Project Mobius/Assets/Scripts/button.cs
+++ Project Mobius/Assets/Scripts/button.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class button : MonoBehaviour {
+public class button : MonoBehaviour
+{
 	//true  = p1 | false = p2
 	public bool p1;
 	//Is AI Player?
-    public bool isAI;
+	public bool isAI;
 	//Keyboard launch key
 	public KeyCode launch = KeyCode.H;
 	//Spawned bar
@@ -20,113 +21,130 @@ public class button : MonoBehaviour {
 	//Sprite Renderer for flips and such
 	private SpriteRenderer sprender;
 	//Camera
-    public Camera mainCamera;
+	public Camera mainCamera;
 	//Color of button and bar
-    public Color color;
+	public Color color;
 	//text for trigger
-    public Text DisplayText;
+	public Text DisplayText;
 	//Beat that will interact with button
-    public GameObject matchedBeat;
+	public GameObject matchedBeat;
+	public bool tapped = false;
 
-    void Awake()
-    {
+	void Awake ()
+	{
 		//Set camera
-        mainCamera = Camera.main;
+		mainCamera = Camera.main;
 		//Get SpriteRenderer
 		sprender = GetComponent<SpriteRenderer> ();
 		//Resize sprite
 		sprender.transform.localScale = sprender.transform.localScale + new Vector3 (scaler, scaler, scaler);
 		//Set display text
-        DisplayText = GetComponent<Text>();
-        DisplayText.GetComponent<Renderer>().sortingLayerID = transform.GetComponent<Renderer>().sortingLayerID;
-    }
+		DisplayText = GetComponent<Text> ();
+		DisplayText.GetComponent<Renderer> ().sortingLayerID = transform.GetComponent<Renderer> ().sortingLayerID;
+	}
 
-    // Use this for initialization
-    void Start () {
-        isAI = GetComponentInParent<player_main>().isai;
+	// Use this for initialization
+	void Start ()
+	{
+		isAI = GetComponentInParent<player_main> ().isai;
 		gameObject.GetComponent<Button> ().onClick.AddListener (() => {
-			shootBar();
+			shootBar ();
 		});	
 	}
 
-    public void claimed()
-    {
-        gameObject.GetComponent<SpriteRenderer>().color = color;
-        beatBar.GetComponent<SpriteRenderer>().color = color;
-    }
+	public void claimed ()
+	{
+		gameObject.GetComponent<SpriteRenderer> ().color = color;
+		beatBar.GetComponent<SpriteRenderer> ().color = color;
+	}
 
-    public void setText()
-    {
-        DisplayText.text = launch.ToString();
-    }
+	public void setText ()
+	{
+		DisplayText.text = launch.ToString ();
+	}
 
-	public void Spawned(bool p1Set){
-        p1 = p1Set;
-        if (beatBar == null)
-        {
-            getBeatBar();
-        }
-        /*
+	public void Spawned (bool p1Set)
+	{
+		p1 = p1Set;
+		if (beatBar == null) {
+			getBeatBar ();
+		}
+		/*
         * HEY CRISTIAN I NEED YOU TO MAKE THE TEXT APPEAR ABOVE THE BUTTON SPRITES!
         * I MADE IT SET TO THE SPECIAL KEY IT GETS TO SERVE ITS FUNCTIONS
         * I DONT KNOW UNITY UI. I STAY HERE. CODE IS SAFE. @_%
         */
         
-    }
-
-	void shootBar(){
-		beatBar.GetComponent<beatBouncer>().hit();
 	}
 
-	void getBeatBar(){
+	void shootBar ()
+	{
+		beatBar.GetComponent<beatBouncer> ().hit ();
+	}
+
+	void getBeatBar ()
+	{
 		beatBar = (GameObject)Instantiate (beatFab);
-        //beatBar.GetComponent<beatBouncer>().parentalUnit = this.gameObject;
-        //Debug.Log("P1: " + p1);
+		//beatBar.GetComponent<beatBouncer>().parentalUnit = this.gameObject;
+		//Debug.Log("P1: " + p1);
 
 		if (p1) {
-            beatBar.transform.position = transform.position + new Vector3(0, barDist * 1 * transform.localScale.y, 0);
-            spawnBar (1);
-		} 
-		else {
-			beatBar.transform.position = transform.position + new Vector3(0, barDist * -1 * transform.localScale.y, 0);
-            spawnBar(-1);
+			beatBar.transform.position = transform.position + new Vector3 (0, barDist * 1 * transform.localScale.y, 0);
+			spawnBar (1);
+		} else {
+			beatBar.transform.position = transform.position + new Vector3 (0, barDist * -1 * transform.localScale.y, 0);
+			spawnBar (-1);
 		}
 
-        beatBar.transform.SetParent(this.transform, true);
+		beatBar.transform.SetParent (this.transform, true);
 	}
 
-	void spawnBar(int aboveBelow){
-        //beatBar.transform.position;
-		beatBar.GetComponent<beatBouncer>().p1 = p1;
+	void spawnBar (int aboveBelow)
+	{
+		//beatBar.transform.position;
+		beatBar.GetComponent<beatBouncer> ().p1 = p1;
 		//beatBar.GetComponent<beatBouncer>().launch = launch;
-		beatBar.GetComponent<beatBouncer>().spawned();
+		beatBar.GetComponent<beatBouncer> ().spawned ();
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		if (Input.GetKeyDown (launch)) {
-			shootBar();
+			shootBar ();
 		}
-        /*if (Input.GetMouseButtonDown (0))
+		/*if (Input.GetMouseButtonDown (0))
 		{
 			if (GetComponent<Collider2D> () == Physics2D.OverlapPoint (Camera.main.ScreenToWorldPoint (Input.mousePosition))) {
 				beatBar.SendMessage("hit");
 			}
 		} */
 
-        var touchCount = Input.touchCount;
-        for (var i = 0; i < touchCount; i++)
-        {
-            var touch = Input.GetTouch(i);
-			if(touch.phase == TouchPhase.Began){
-		        if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(touch.position)))
-		        {
-		            beatBar.GetComponent<beatBouncer>().hit ();
-		        }
+		var touchCount = Input.touchCount;
+		for (var i = 0; i < touchCount; i++) {
+			var touch = Input.GetTouch (i);
+			if (touch.phase == TouchPhase.Began) {
+				if (!tapped) {
+					if (GetComponent<Collider2D> () == Physics2D.OverlapPoint (Camera.main.ScreenToWorldPoint (touch.position))) {
+						tapped = true;
+						beatBar.GetComponent<beatBouncer> ().hit ();
+					}
+				}
 			}
-        }
-        //Debug.Log("SCREENPOS: " + screenPos);
-        //Debug.Log("WORLDPOS: " + worldPos);
+			if (touch.phase == TouchPhase.Moved) {
+				if (!tapped) {
+					if (GetComponent<Collider2D> () == Physics2D.OverlapPoint (Camera.main.ScreenToWorldPoint (touch.position))) {
+						tapped = true;
+						beatBar.GetComponent<beatBouncer> ().hit ();
+					}
+				}
+			}
+			if (touch.phase == TouchPhase.Ended) {
+				tapped = false;
+			}
+		}
+		//Debug.Log("SCREENPOS: " + screenPos);
+		//Debug.Log("WORLDPOS: " + worldPos);
 
-    }
+	}
 }
